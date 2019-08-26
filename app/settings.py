9f5267f54b9c81f69344bfe7108f1f93992792fb
Joji Varghese <jojimpv@gmail.com>
2019-08-26 11:35:15 +0530

    qinfo init

--- app/settings.py
+++ app/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'ui', 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'ui', 'build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'ui', 'build', 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ui', 'build', 'static')
+]
