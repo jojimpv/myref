14168da2bf0b95e96c11bd7732122d81997dfa5c
jojimpv <jojimpv@gmail.com>
2019-09-05 00:22:59 +0530

    qinfo ui and api updates (#2)
    * qinfo ui style change and api enhancements

--- app/conf.py
+++ app/conf.py
@@ -1,1 +1,4 @@
-QINFO_DATA_DIR = '/home/joji/Documents/daily'
+import os
+
+DEFAULT_DATA_DIR = '/home/joji/Documents/daily'
+QINFO_DATA_DIR = os.getenv('MYREF_DATA_DIR', DEFAULT_DATA_DIR)
--- qinfo/views.py
+++ qinfo/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from app import conf
 from glob import glob
+from urllib.parse import parse_qs
 
 logger = logging.getLogger(__name__)
 
@@ -13,15 +14,16 @@ def index(request):
 
 
 def query(request):
-    txt = request.GET.urlencode().partition('txt=')[2]
-    flist = glob('{d}/**/*.yml'.format(d=conf.QINFO_DATA_DIR), recursive=True)
+    txt = parse_qs(request.GET.urlencode(), keep_blank_values=True)['txt'][0].lower()
+    flist = glob('{d}/**/*.*'.format(d=conf.QINFO_DATA_DIR), recursive=True)
+    flist.sort(reverse=True)
     result = []
     for f in flist:
         with open(f) as fdata:
             lines = fdata.readlines()
             filematch = dict(filename=f, matches=[])
             for lno, line in enumerate(lines, start=1):
-                if txt in line:
+                if txt in line.lower():
                     filematch['matches'].append(dict(lno=lno, txt=line))
             if filematch['matches']:
                 result.append(filematch)
